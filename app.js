5cf0d095bade73fff4ce89f751294692b6776ab6
mbair87 <91098965+mbair87@users.noreply.github.com>
2022-01-29 13:22:43 -0700

    added addRole

--- app.js
+++ app.js
@@ -105,7 +105,38 @@ const addDepartment = () => {
 };
 
 //add a role addRole
+const addRole = () => {
+    inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "What role would you like to add?"
 
+
+    },{
+        type: "input", 
+        name: "salary",
+        message: "What is the salary for the role?"
+    
+    }, {
+        type: "list",
+        name: "department_id", 
+        message: "What is the department ID that this new role will belong to?",
+        choices: ["1", "2", "3", "4", "5"]
+
+    }]
+    ).then(function (data){
+        
+        connectToDb.query('INSERT INTO role SET ?', {
+            title: data.title,
+            salary: data.salary,
+            department_id: data.department_id
+        },
+        function (err, res) {if (err) throw err
+        console.log("New Role Added")
+        menu()
+    }
+        )
+})}
 //add an employee addEmployee
 
 //update employee role  updateEmployeeRole
