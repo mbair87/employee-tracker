659fe1628c01f85f0f99066c4aed0d4e7d7255ff
mbair87 <91098965+mbair87@users.noreply.github.com>
2022-01-29 14:17:54 -0700

    updated addRole to get departments, added addEmployee

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
 // Connect to database
-const connectToDb = mysql.createConnection({
-  host: "localhost",
-  // Your MySQL username,
-  user: "root",
-  // Your MySQL password
-  password: "",
-  database: "employee_DB",
-},
-console.log('connected to employee_db')
+const connectToDb = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "",
+    database: "employee_DB",
+  },
+  console.log("connected to employee_db")
 );
-connectToDb.connect(function (err){
-if(err) throw err
+connectToDb.connect(function (err) {
+  if (err) throw err;
 
-startPrompts();
-
+  menu();
 });
 
 // starting menu prompt
@@ -43,7 +43,7 @@ function menu() {
       if (data.choice === "View All Employees") {
         viewAllEmployees();
       } else if (data.choice === "Add Employee") {
-        addEmployees();
+        addEmployee();
       } else if (data.choice === "Update Employee Role") {
         updateEmployeeRole();
       } else if (data.choice === "View All Departments") {
@@ -111,37 +111,91 @@ const addDepartment = () => {
 
 //add a role addRole
 const addRole = () => {
-    inquirer.prompt([{
-        type: "input",
-        name: "title",
-        message: "What role would you like to add?"
-
-
-    },{
-        type: "input", 
-        name: "salary",
-        message: "What is the salary for the role?"
-    
-    }, {
-        type: "list",
-        name: "department_id", 
-        message: "What is the department ID that this new role will belong to?",
-        choices: ["1", "2", "3", "4", "5"]
-
-    }]
-    ).then(function (data){
-        
-        connectToDb.query('INSERT INTO role SET ?', {
-            title: data.title,
-            salary: data.salary,
-            department_id: data.department_id
+  connectToDb.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What role would you like to add?"
         },
-        function (err, res) {if (err) throw err
-        console.log("New Role Added")
-        menu()
-    }
-        )
-})}
+        {
+            type: "input", 
+            name: "salary",
+            message: "What is the salary for the role?"
+        },
+        {
+          type: "list",
+          name: "department_id",
+          message: "What department does this new role belong to?",
+          choices: res.map((department) => department.name),
+        },
+      ])
+      .then(function (data) {
+        const departmentName = res.find((department) => department.name === data.department_id);
+        connectToDb.query(
+          "INSERT INTO role SET ?",
+          {
+            title: data.title,
+          salary: data.salary,
+            department_id: departmentName.id,
+          },
+          function (err, res) {
+            if (err) throw err;
+            console.log("New role added");
+            menu();
+          }
+        );
+      });
+  });
+};
 //add an employee addEmployee
+
+const addEmployee = () => {
+  connectToDb.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "first_name",
+          type: "input",
+          message: "What is the employee's first name?",
+        },
+        {
+          name: "last_name",
+          type: "input",
+          message: "What is the employee's last name?",
+        },
+        {
+          type: "list",
+          name: "role_id",
+          message: "What is the role of the new employee?",
+          choices: res.map((role) => role.title),
+        },
+        {
+          name: "manager_name",
+          type: "list",
+          message: "What is the ID of the employee's manager?",
+        },
+      ])
+      .then(function (data) {
+        const roleTitle = res.find((role) => role.title === data.role_id);
+        connectToDb.query(
+          "INSERT INTO employee SET ?",
+          {
+            first_name: data.first_name,
+            last_name: data.last_name,
+            role_id: roleTitle.id,
+          },
+          function (err, res) {
+            if (err) throw err;
+            console.log("Added new employee");
+            menu();
+          }
+        );
+      });
+  });
+};
 
 //update employee role  updateEmployeeRole
