02fe1c88d122da1e3b5146e8711673d8a3b4f83b
mbair87 <91098965+mbair87@users.noreply.github.com>
2022-01-29 11:49:40 -0700

    added addDepartment

--- app.js
+++ app.js
@@ -3,81 +3,109 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
 // Connect to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
-    // Your MySQL password
-    password: '',
-    database: 'employee_DB'
+const connectToDb = mysql.createConnection({
+  host: "localhost",
+  // Your MySQL username,
+  user: "root",
+  // Your MySQL password
+  password: "",
+  database: "employee_DB",
+},
+console.log('connected to employee_DB')
+);
+menu();
+
+// starting menu prompt
+
+function menu() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "choice",
+      message: "What would ou like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Departments",
+        "Add Department",
+        "View All Roles",
+        "Add Role",
+        "Quit",
+      ],
+    })
+    .then(function (data) {
+      if (data.choice === "View All Employees") {
+        viewAllEmployees();
+      } else if (data.choice === "Add Employee") {
+        addEmployees();
+      } else if (data.choice === "Update Employee Role") {
+        updateEmployeeRole();
+      } else if (data.choice === "View All Departments") {
+        viewAllDepartments();
+      } else if (data.choice === "Add Department") {
+        addDepartment();
+      } else if (data.choice === "View All Roles") {
+        viewAllRoles();
+      } else if (data.choice === "Add Role") {
+        addRole();
+      } else if (data.choice === "Quit") {
+        console.log("Goodbye!");
+      }
+    });
+}
+
+//view all departments
+const viewAllDepartments = () => {
+  connectToDb.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    menu();
   });
+};
 
-  // starting menu prompt
+//view all roles viewAllRoles
+const viewAllRoles = () => {
+  connectToDb.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    menu();
+  });
+};
 
-  function menu() {
-    inquirer.prompt(
+//view all employees viewAllEmployees
+const viewAllEmployees = () => {
+  connectToDb.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    menu();
+  });
+};
+//add a department addDepartment
+const addDepartment = () => {
+  inquirer
+    .prompt({
+      type: "input",
+      name: "dept",
+      message: "Which department would you like to add?",
+    })
+    .then(function (data) {
+      connectToDb.query(
+        "INSERT INTO department SET ?",
         {
-            type: "list", 
-            name: "choice", 
-            message: "What would ou like to do?",
-            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Departments", "Add Department", "View All Roles", "Add Role", "Quit"]
+          name: data.dept,
+        },
+        function (err, res) {
+          if (err) throw err;
+          console.log("New department added");
+          menu();
         }
-        ).then(function (data) {
-            if (data.choice === "View All Employees") {
-                viewAllEmployees();
-            } else if (data.choice === "Add Employee") {
-                addEmployees();
-            } else if (data.choice === "Update Employee Role") {
-                updateEmployeeRole();
-            } else if (data.choice === "View All Departments") {
-                viewAllDepartments();
-            } else if (data.choice === "Add Department") {
-                addDepartment();
-            } else if (data.choice === "View All Roles") {
-                viewAllRoles();
-            } else if (data.choice === "Add Role") {
-                addRole()
-            } else if (data.choice === "Quit") {
-                console.log("Goodbye!")
-            }
-        })
-    }
+      );
+    });
+};
 
+//add a role addRole
 
-  //view all departments
-  const viewAllDepartments = () => {
-    connectToDb.query('SELECT * FROM department', function (err, res) {
-        if(err) throw err;
-        console.table(res);
-        menu();
-    })
-}
+//add an employee addEmployee
 
-
-  //view all roles viewAllRoles
-  const viewAllRoles = () => {
-    connectToDb.query('SELECT * FROM role', function (err, res) {
-        if(err) throw err;
-        console.table(res);
-        menu();
-    })
-}
-
-  //view all employees viewAllEmployees
-  const viewAllEmployees = () => {
-    connectToDb.query('SELECT * FROM employee', function (err, res) {
-        if(err) throw err;
-        console.table(res);
-        menu();
-    })
-}
-  //add a department addDepartment
-
-
-  //add a role addRole
-
-
-  //add an employee addEmployee
-
-
-  //update employee role  updateEmployeeRole
+//update employee role  updateEmployeeRole
--- schema.sql
+++ schema.sql
@@ -3,13 +3,14 @@ DROP DATABASE IF EXISTS employee_DB;
 CREATE DATABASE employee_DB;
 USE employee_DB;
 
---department table
+DROP TABLE IF EXISTS department;
+-- department table
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
-
---role table
+DROP TABLE IF EXISTS role;
+-- role table
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
@@ -18,14 +19,14 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
---employee table
+-- employee table
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
